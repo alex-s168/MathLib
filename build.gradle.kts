86c1e88067e5b58633f485167d273c41636c4d6e
alex_s168 <63254202+alex-s168@users.noreply.github.com>
2023-12-27 14:26:13 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("maven-publish")
+}
+
+group = "me.alex_s168"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
